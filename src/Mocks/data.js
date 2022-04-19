fef21bb58de05ff67ef730539e38b78411122ce9
Harrison <harri.torres12@gmail.com>
2022-04-18 22:37:20 -0300

    frontend-complet

---
+++ src/Mocks/data.js
@@ -0,0 +1,36 @@
+export const data = [
+  {
+    id: 1,
+    name: "Harry Potter",
+    house: "Gryffindor",
+    actor: "Daniel Radcliffe",
+    image: "http://hp-api.herokuapp.com/images/harry.jpg",
+  },
+  {
+    id: 2,
+    name: "Hermione Granger",
+    house: "Gryffindor",
+    actor: "Emma Watson",
+    image: "http://hp-api.herokuapp.com/images/hermione.jpeg",
+  },
+  {
+    id: 3,
+    name: "Ron Weasley",
+    house: "Gryffindor",
+    actor: "Rupert Grint",
+    image: "http://hp-api.herokuapp.com/images/ron.jpg",
+  },
+  {
+    id: 4,
+    name: "Draco Malfoy",
+    house: "Slytherin",
+    actor: "Tom Felton",
+    image: "http://hp-api.herokuapp.com/images/draco.jpg",
+  },
+  {
+    id: 5,
+    name: "Minerva McGonagall",
+    house: "Gryffindor",
+    actor: "Dame Maggie Smith",
+    image: "http://hp-api.herokuapp.com/images/mcgonagall.jpg",
+  }]
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,32 @@
+import { useState, useEffect } from "react";
+
+import { Card } from "../../component/Card/Card";
+import { data } from "../../Mocks/data";
+
 import "./home.css";
 
 export function Home() {
-  return <h2>Home</h2>;
+  const [characters, setCharacters] = useState([]);
+  
+  useEffect(() => {
+    setCharacters(data)
+  }, [])
+
+  return (
+    <>
+      <section className="cards-home">
+        {characters.map((char) => {
+          return (
+            <Card
+              key={"char_${char.id}"}
+              name={char.name}
+              house={char.house}
+              image={char.image}
+              actor={char.actor}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
 }
