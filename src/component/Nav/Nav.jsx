9ad1ee4bec9566e51d88d071459e9ce579bc58d9
Harrison <harri.torres12@gmail.com>
2022-04-18 21:27:32 -0300

    css_nav_footer

--- src/component/Nav/Nav.jsx
+++ src/component/Nav/Nav.jsx
@@ -1,17 +1,28 @@
-import './Nav.css'
-import { Link } from 'react-router-dom'
+import "./Nav.css";
+import { Link } from "react-router-dom";
+import Icon from "../../images/icon.png";
 
-export function Nav(){
-    return(
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/About">About</Link>
-                </li>
-            </ul>
-        </nav>
-    )
+export function Nav() {
+  return (
+    <nav>
+      <ul>
+        <div className="nav-title">
+          <li>
+            <img src={Icon} alt="Icon HP" />
+          </li>
+          <li>
+            <h1>Personagens do Harry Potter</h1>
+          </li>
+        </div>
+        <div className="nav-Links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/About">About</Link>
+          </li>
+        </div>
+      </ul>
+    </nav>
+  );
 }
